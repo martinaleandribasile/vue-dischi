3981aaf4c94b554460b9f22a3bcf81f12e9cc137
MartinaBasile <martinaleandribasile@gmail.com>
2022-09-27 16:19:58 +0200

    header select non funziona

--- src/components/CdMain.vue
+++ src/components/CdMain.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <NavbarComponent/>
     <div class="container-list">
         <div class="CdList" >
             <CdCard v-for="cd in CdArray" :key="cd.title" :cd='cd'/>
@@ -11,41 +10,34 @@
 
 <script>
     import CdCard from '@/components/CardCd.vue'
-    import NavbarComponent from "@/components/NavbarCd.vue"
 export default {
     name:'CdMain',
-    data(){
-        return{
-            
-        }
-    },
-    
     props:{
         CdArray:Array,
     },
     components:{
         CdCard,
-        NavbarComponent
     }
 
 }
 </script>
 
 <style lang="scss" scoped>
+   
     .container{
         height: calc(100% - 10%);
         background-color: #1e2d3b;
     }
-.container-list{
-    height: calc(100% - 20%);
-    background-color: #1e2d3b;
-    display: flex;
-    align-items: center;
-    .CdList{
+    .container-list{
+        height: calc(100% - 20%);
+        background-color: #1e2d3b;
         display: flex;
-        flex-wrap: wrap;
-        max-width: 1800px;
-        margin: 0 auto;
+        align-items: center;
+        .CdList{
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 1800px;
+            margin: 0 auto;
+        }
     }
-}
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,56 @@
 <template>
-  <div class="container">
-    <div class="containerLogo"><img src="https://ajourneyintosound.de/wp-content/uploads/2019/02/spotify_logo.png" alt="spotify-logo-small"></div>
+ <div class="header"> 
+    <div class="container">
+      <div class="containerLogo"><img src="https://ajourneyintosound.de/wp-content/uploads/2019/02/spotify_logo.png" alt="spotify-logo-small"></div>
+    </div>
+    <div class="container-sel">
+    <div class="selection-container">
+        <h3>Filtra per genere</h3>
+        <select name="GenereCd"  @change="onChange($event)">
+            <option value="All">All</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
+            <option value="Pop">Pop</option>
+            <option value="Rock">Rock</option>
+        </select>
+    </div>
+    <div class="selection-container">
+        <h3>Filtra per genere</h3>
+        <select name="AuthorCd" id="">
+            <option value="All">All</option>
+            <option value="Bon Jovi">Bon Jovi</option>
+            <option value="Sting">Sting</option>
+            <option value="Queen">Queen</option>
+            <option value="Steve Gadd Band">Steve Gadd Band</option>
+            <option value="Iron Maiden">Iron Maiden</option>
+            <option value="Eric Clapton">Eric Clapton Maiden</option>
+            <option value="Deep Purple">Deep Purple</option>
+            <option value="Metallica">Metallica</option>
+            <option value="Dave Weckl">Dave Weckl</option>
+            <option value="Michael Jacjson">Michael Jackson</option>
+        </select>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
     name: 'HeaderComponent',
+    data(){
+        return{
+            value:''
+        }
+    },
+    methods:{
+    onChange(event){
+        console.log(event)
+        console.log('evento' ,event.target.value)
+        this.value = event.target.value
+        console.log('value' ,this.value)
+        this.$emit('adminsel' , this.value)
+    }
+},
 }
 </script>
 
@@ -25,4 +69,28 @@ export default {
     }
 }
 }
+.container-sel{
+        height: 10%!important;
+        padding-right: 100px;
+        background-color: #1e2d3b;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-end;
+        column-gap: 20px;
+        .selection-container{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            h3{
+            color: white;
+            padding-right: 10px;
+            }
+            select{
+                padding: 5px;
+                width: 100px;
+            }
+        }
+      
+    }
 </style>
